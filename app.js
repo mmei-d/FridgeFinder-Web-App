ce0d8a8ce82487e45f409283c0d48cad31989a2c
mmei-d <meideng06@gmail.com>
2022-06-28 16:01:07 -0400

    Added popular recipes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const searchInput = document.querySelector('#search-input')
 const addButton = document.querySelector('#add')
 let numIngred = 0;
 
-// add ingredients
+// add and remove ingredients
 const addIngred = () => {
     // increment number of ingredients by 1
     numIngred++
@@ -45,4 +45,55 @@ const addIngred = () => {
 }
 
 addButton.addEventListener('click', addIngred)
+
+// add home page popular recipes
+const recipeSection = document.querySelector('#all-recipes')
+
+for(let i = 0; i < 12; i++){
+    // recipe card
+    const recipeCard = document.createElement('div')
+    recipeSection.append(recipeCard)
+    recipeCard.classList.add('one-recipe')
+
+    // card image
+    const image = document.createElement('img')
+    let imageVal = 'images/food-example.jpeg'
+    image.setAttribute('src', imageVal)
+    image.setAttribute('alt', 'food photo')
+    recipeCard.append(image)
+
+    // recipe title
+    const title = document.createElement('h3')
+    let titleVal = 'Recipe Title'
+    title.innerHTML = titleVal
+    recipeCard.append(title)
+
+    // cuisine
+    const cuisine = document.createElement('p')
+    cuisine.classList.add('cuisine')
+    let cuisineVal = 'Chinese'
+    cuisine.innerHTML = 'Cuisine: ' + cuisineVal
+    recipeCard.append(cuisine)
+
+    // health score
+    const healthScore = document.createElement('p')
+    healthScore.classList.add('health-score')
+    let healthScoreVal = 10
+    healthScore.innerHTML = 'Health Score: ' + healthScoreVal
+    recipeCard.append(healthScore)
+
+    // cook time
+    const cookTime = document.createElement('p')
+    cookTime.classList.add('cook-time')
+    let cookTimeVal = '20 min'
+    cookTime.innerHTML = 'Cook Time: ' + cookTimeVal
+    recipeCard.append(cookTime)
+
+    // servings
+    const servings = document.createElement('p')
+    servings.classList.add('servings')
+    let servingsVal = 2
+    servings.innerHTML = 'Servings: ' + servingsVal
+    recipeCard.append(servings)
+}
 
