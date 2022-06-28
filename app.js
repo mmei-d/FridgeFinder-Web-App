53f55cac9652aa76d014b57f045f15e8fb51c766
mmei-d <meideng06@gmail.com>
2022-06-27 23:37:47 -0400

    Add ingredients to pantry

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-// div of added ingredients
 const ingredDiv = document.querySelector('#ingred-buttons')
-
-// ingred input
 const searchInput = document.querySelector('#search-input')
-
-// ingred add button
 const addButton = document.querySelector('#add')
+let numIngred = 0;
 
-
-// add ingredients to ingredients div
+// add ingredients
 const addIngred = () => {
+    // increment number of ingredients by 1
+    numIngred++
+
+    // make "no ingredients added yet" text disappear
+    const noIngred = document.querySelector('#no-ingred')
+    noIngred.classList.add('hidden')
+
     // make ingredient button
     const ingred = document.createElement('button')
+    ingredDiv.append(ingred)
 
-    // add x (delete ingred) to ingredient button
+    // add "x" to ingredient button
     const x = document.createElement('img')
     x.setAttribute('src', 'images/x-close.svg')
     ingred.append(x)
 
+    // add button text
+    const btnText = document.createElement('p')
     if(searchInput.value === ''){
         alert("Please enter an ingredient :)");
-    // add ingredient button to div w/ ingredients
     }else{
-        ingred.innerHTML = searchInput.value
-        ingredDiv.append(ingred)
+        btnText.innerHTML = searchInput.value
     }
+    ingred.append(btnText)
 
     // clear input field after adding ingredient
     searchInput.value = ''
+
+    // delete ingredient
+    ingred.addEventListener('click', () => {
+        ingred.remove()
+        numIngred--
+        // if all ingredients have been removed, show "no ingred in pantry" message again
+        if(numIngred === 0){
+            noIngred.classList.remove('hidden')
+        }
+    })
 }
 
 addButton.addEventListener('click', addIngred)
+
